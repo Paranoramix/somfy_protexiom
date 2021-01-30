46eaf5b5ce8c5c9ea2f72c618c5ec03556ffe53b
Paranoramix <gilles.leonis@gmail.com>
2021-01-30 06:37:40 +0100

    remove const

--- custom_components/somfy_protexiom/__init__.py
+++ custom_components/somfy_protexiom/__init__.py
@@ -17,8 +17,6 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 from .const import (
     CONF_PASSWORD,
     CONF_USERNAME,
-    DOMAIN,
-    PLATFORMS,
     STARTUP_MESSAGE,
 )
 
